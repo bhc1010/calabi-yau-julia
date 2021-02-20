060fead47ade91218009b81a5605218700c46151
bhc1010 <61128244+bhc1010@users.noreply.github.com>
2021-02-20 09:27:51 -0500

    Update sampleData.jl

--- src/sampleData.jl
+++ src/sampleData.jl
@@ -1,26 +1,22 @@
 using DataFrames, CSV, FileIO, CodecZlib
 
-const TOTAL_DATA = 473800746
+const TOTAL_DATA = 473_800_746
 const TOTAL_NUM_DB = 474
 const DIM = 4
-const sampleSize = 1_000_000
+SAMPLE_SIZE = 1_000_000
 
-function LoadDB(index::Integer)
-    df = DataFrame(load(File(format"CSV", "/media/share/Dev/CalabiYau/data/polytopes_db_4d/$index.gz")))
-    return df
-end
 
 function RandomSample(sampleSize::Integer)
     binnedSample = []
     sample = rand(1:TOTAL_DATA, sampleSize)
     bins = [x for x in 0:sampleSize:TOTAL_DATA]
     push!(bins, TOTAL_DATA)
-
+    
     sort!(sample)
     for i in 2:length(bins)
         subsample = []
         for (j,X) in enumerate(sample)
-
+            
             if bins[i-1] < X ≤ bins[i]
                 push!(subsample, X) 
             else 
@@ -30,7 +26,7 @@ function RandomSample(sampleSize::Integer)
         end
         if !isempty(subsample) push!(binnedSample, subsample);end;
     end
-
+    
     return convert(Array{Array{Int32}}, binnedSample)
 end
 
@@ -42,15 +38,22 @@ function GetDFMatrix(df::DataFrame, rowHead::Integer, rowFoot::Integer, colHead:
     return df[rowHead:rowFoot, colHead:colFoot]
 end
 
-function SearchDB(df::DataFrame, required::Array, dbId::Integer)
-    sort!(required)
-    println("Database: ", dbId, "\n REQUIRED: $(length(required))")
+SetSampleSize!(sampleSize::Integer) = global SAMPLE_SIZE = sampleSize;
 
+SetPopulationSize!(popSize::Integer) = global TOTAL_DATA = popSize;
+
+function DataFrameToArray(df::DataFrame, dbId::Integer;required=[], reimport=false)
+    dfPolyId = (dbId - 1) * SAMPLE_SIZE + 1
     data = []
     currentRow = 1;
-    dfPolyId = (dbId - 1) * sampleSize;
     skip = 0;
     dfSize = size(df,1)
+    if reimport == true
+        required = [x for x in 1:SAMPLE_SIZE]
+        dfPolyId = 1
+    elseif isempty(required)
+        required = [x for x in ((dbId - 1) * SAMPLE_SIZE + 1 ):(dbId * SAMPLE_SIZE)]
+    end
     for reqPolyId in required
         while(reqPolyId != dfPolyId && currentRow < dfSize)
             #get skip
@@ -66,8 +69,9 @@ function SearchDB(df::DataFrame, required::Array, dbId::Integer)
         ontology = GetDFValue(df, currentRow + 1, 1, Int32)
         push!(data, (polyMat,ontology))
     end
-    println("GOT: $(length(data))")
-    println(length(data) == length(required))
+    println("Database: ", dbId) 
+    println("REQUIRED: $(length(required))")
+    println("GOT: $(length(data)) ", length(data) == length(required))
     return convert(Array{Tuple{Array{Float64,2},Int32}}, data)
 end
 
@@ -85,30 +89,96 @@ function ExportData(data, path)
     end
     
     open(path, "w") do io
-        println("Opening ", path)
+        @info("Opening ", path)
         stream = GzipCompressorStream(io)
-        println("Writing to path...")
+        @info("Writing to path...")
         CSV.write(stream, dataOut)
         close(stream)
-        println("Data exported.")
+        @info("Data exported.")
     end
 end
 
-function GetDataSample(required::Array{Array{Int32}}; num_db::Integer = TOTAL_NUM_DB, export_path="None") 
+function SplitAndExport(data, id, path; train_percent=0.8)
+    train_set = []
+    test_set = []
+    @info("Splitting data...")
+    for (i,d) in enumerate(data)
+        if i/size(data,1) < train_percent
+            push!(train_set, d)
+        else
+            push!(test_set, d)
+        end
+    end
+    n = 0
+    while(!isdir(path*"/$n"))
+        n += 1
+    end
+    try
+        mkdir(path*"/$n")
+    catch e
+        throw(e)
+        @warn("DATA NOT EXPORTED: NO DIRECTORY CREATED.")
+    end
+    path = path*"/$n"
+    ExportData(train_set, path*"/train.gz")
+    ExportData(test_set, path*"/test.gz")
+end
+
+function GetNewSample(;required=nothing, db_path="/media/share/Dev/CalabiYau/data/polytopes_db_4d", export_path="None", split=true, train_percent=0.8,num_db::Integer = TOTAL_NUM_DB) 
+    if required ≡ nothing required = RandomSample(SAMPLE_SIZE); end;
+    
     fullData = []
     for i in 1:num_db
-        df = LoadDB(i);
-        dbData = SearchDB(df, required[i], i)
+        df = LoadDB(db_path, i, catch_errors=true);
+        dbData = DataFrameToArray(df, i, required=required[i])
         append!(fullData, dbData)
     end
-    println("OBTAINED: $(size(fullData,1)) IN TOTAL")
-    if export_path != "None" ExportData(fullData, export_path);else println("DATA NOT EXPORTED: NO export_path GIVEN."); end
+    @info("OBTAINED: $(size(fullData,1)) IN TOTAL")
+    
+    if export_path != "None" 
+        try
+            split == true ? SplitAndExport(fullData, export_path, train_percent=train_percent) : ExportData(fullData, export_path)
+        catch error
+            throw(error)
+            @error("DATA NOT EXPORTED.")
+        end
+    else 
+        @warn("DATA NOT EXPORTED: NO export_path GIVEN.")
+    end
     return fullData
 end
 
-function __main__()  
-    required = RandomSample(sampleSize)
-    fullData = GetDataSample(required, export_path="/home/oppenheimer/Dev/calabiyau/models/data/1.gz")
+function LoadDB(path, index::Integer; train_data=false, test_data=false, catch_errors=false)
+    if catch_errors == true
+        try
+            FIRST_IMPORT = DataFrame(load(File(format"CSV", "/home/oppenheimer/Dev/calabiyau/models/data/1.gz")))
+        catch
+            @info "Precompiling packages, ignoring errors..."
+        end
+    end
+    
+    if train_data
+        return DataFrame(load(File(format"CSV", path*"/$index/train.gz")))
+    elseif test_data
+        return DataFrame(load(File(format"CSV", path*"/$index/test.gz")))
+    else
+        return DataFrame(load(File(format"CSV", path*"/$index.gz")))
+    end
 end
 
-__main__()
+function ImportData(import_path::String, dbId::Integer; train_data=false, test_data=false, catch_errors=false)
+    df = LoadDB(import_path, dbId, train_data=train_data, test_data=test_data ,catch_errors=catch_errors)
+    data = DataFrameToArray(df, dbId, reimport=true)
+    @info("IMPORTED ", size(df,1), " POLYTOPES.")
+    return data
+end
+
+function ImportSplitData(import_path::String, dbId::Integer; catch_errors=false)
+    train_df = LoadDB(import_path, dbId, train_data=true, catch_errors=catch_errors)
+    test_df = LoadDB(import_path, dbId, test_data=true)
+    train_data = DataFrameToArray(train_df, dbId, reimport=true)
+    test_data = DataFrameToArray(test_df, dbId, reimport=true)
+    @info("IMPORTED ", size(train_data,1), " POLYTOPES FOR TRAINING.")
+    @info("IMPORTED ", size(test_data,1), " POLYTOPES FOR TESTING.")
+    return train_data, test_data
+end
